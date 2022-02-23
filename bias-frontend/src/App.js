5309c63f834677bf502799c2222edc55c56af1c0
Chaz <zhou@linux.com>
2022-02-22 21:50:44 -0600

    Update design

--- bias-frontend/src/App.js
+++ bias-frontend/src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "antd/dist/antd.css";
+import "./App.css"
 import { useState } from "react";
-import { Typography, Row, Col, Input } from "antd";
+import { Typography, Row, Col, Input, Menu, Breadcrumb, Divider } from "antd";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { reorderStatement } from "./reorder";
 import { WordList } from "./WordList.tsx";
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   const { Title } = Typography;
@@ -18,14 +21,30 @@ function App() {
 
   return (
     <>
+    <Layout className="layout">
+    <Header>
+      <div className="logo">BiasExaminer</div>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['Examine']}>
+        <Menu.Item key="Examine">Examine</Menu.Item>
+        <Menu.Item key="Settings">Settings</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>BiasExaminer</Breadcrumb.Item>
+        <Breadcrumb.Item>Examine</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-content">
       <Row align="middle">
         <Col offset={10}>
-          <Title>CS 338 Bias Project</Title>
           <Search
             placeholder="Please enter a statement"
             enterButton="Submit"
             onSearch={onSubmit}
           />
+        </Col>
+        <Divider></Divider>
+        <Col offset={10}>
           <DragDropContext
             onDragEnd={({ destination, source }) => {
               if (!destination) {
@@ -47,8 +66,12 @@ function App() {
               ))}
             </div>
           </DragDropContext>
-        </Col>
+          </Col>
       </Row>
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>BiasExaminer ©2022</Footer>
+  </Layout>
     </>
   );
 }
