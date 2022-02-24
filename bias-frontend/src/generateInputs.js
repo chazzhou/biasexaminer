7583cf4526ef3a6778354f0677ca6aa7d9dbbabd
oliviergabison <olivier.gabison@gmail.com>
2022-02-23 23:05:20 -0600

    fixed bug

--- bias-frontend/src/generateInputs.js
+++ bias-frontend/src/generateInputs.js
@@ -1,49 +1,46 @@
-
-
 export const generateInputs = (inputs) => {
+  const masks = [
+    "{companies}",
+    "{race singular}",
+    "{race plural}",
+    "{jobs}",
+    "{nationalities}",
+  ];
 
-    const masks = ["{companies}","{race singular}","{race plural}", "{jobs}", "{nationalities}" ];
+  const dict = {
+    "{race plural}": "./race_plural.json",
+    "{race singular}": "./race_singular.json",
+    "{companies}": "./companies.json",
+    "{jobs}": "./jobs.json",
+    "{nationalities}": "./nationalities.json",
+  };
 
-    const dict = {
-        "{race plural}": './race_plural.json',
-        "{race singular}": './race_singular.json',
-        "{companies}": './companies.json',
-        "{jobs}": './jobs.json',
-        "{nationalities}": './nationalities.json'
+  const result = inputs.filter((word) => masks.includes(word));
+
+  let files = [];
+  result.forEach((mask) => files.push(dict[mask]));
+
+  let json = require(`${files[0]}`);
+  const items = json.items;
+  let sentences = [];
+
+  for (let i = 0; i < result.length; i++) {
+    let index = inputs.indexOf(result[i]);
+    for (let j = 0; j < items.length; j++) {
+      let begin = inputs.slice(0, index);
+      let end = inputs.slice(index + 1);
+
+      begin.push(items[j]);
+      const final = begin.concat(end);
+
+      let sentence = final.join(" ");
+      sentence = capitalizeFirstLetterAndAppendPeriod(sentence);
+      sentences.push(sentence);
     }
-
-
-    const result = inputs.filter(word => masks.includes(word));
-
-
-    let files = [];
-    result.forEach(mask => files.push(dict[mask]));
-
-    
-    let json = require(`${files[0]}`);
-    const items = json.items;
-
-    let sentences = [];
-
-    for(let i = 0; i< result.length; i++){
-        let index = inputs.indexOf(result[i]);
-        for(let j = 0; j< items.length; j++){
-
-            if (index !== -1) {
-
-                inputs[index] = items[j];
-            }
-            let sentence = inputs.join(" ");
-            sentence = capitalizeFirstLetterAndAppendPeriod(sentence);
-            sentences.push(sentence);
-
-        }
-    }
-    return sentences;
-
-    
+  }
+  return sentences;
 };
 
 const capitalizeFirstLetterAndAppendPeriod = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1) + ".";
-  };
+  return string.charAt(0).toUpperCase() + string.slice(1) + ".";
+};
